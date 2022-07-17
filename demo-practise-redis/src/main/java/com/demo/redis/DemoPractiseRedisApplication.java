8edbe1b613a8cff6ec0256adfdd489f29ffb9380
jiang <jiangyw2@haid.com.cn>
2022-07-18 01:12:20 +0800

    Redis研究

---
+++ demo-practise-redis/src/main/java/com/demo/redis/DemoPractiseRedisApplication.java
@@ -0,0 +1,13 @@
+package com.demo.redis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoPractiseRedisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoPractiseRedisApplication.class, args);
+    }
+
+}
