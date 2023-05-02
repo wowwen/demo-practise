aad8aa145ba35a5d7496303bd82fb3bc35649b45
jans9 <918418701@qq.com>
2023-05-03 04:34:39 +0800

    Stream相关操作-3

--- demo-practise-provider-1/src/test/java/com/demo/practise/stream/streamtest/StreamTerminalOperationCollectorPractice.java
+++ demo-practise-provider-1/src/test/java/com/demo/practise/stream/streamtest/StreamTerminalOperationCollectorPractice.java
@@ -202,8 +202,7 @@ public class StreamTerminalOperationCollectorPractice {
     @Test
     public void averagingInt() {
         //使用collector实现求平均值
-        log.info("[1, 2, 3, 4, 5, 6]的平均值：{}",
-                Stream.of(1, 2, 3, 4, 5, 6).collect(Collectors.averagingInt(n -> n)));
+        log.info("[1, 2, 3, 4, 5, 6]的平均值：{}", Stream.of(1, 2, 3, 4, 5, 6).collect(Collectors.averagingInt(n -> n)));
     }
 
     /**
@@ -227,8 +226,7 @@ public class StreamTerminalOperationCollectorPractice {
     @Test
     public void summingInt(){
         //使用Collector获取总和
-        log.info("[1, 2, 3, 4, 5, 6]的总和：{}",
-                Stream.of(1, 2, 3, 4, 5, 6).collect(Collectors.summingInt(n -> n)));
+        log.info("[1, 2, 3, 4, 5, 6]的总和：{}", Stream.of(1, 2, 3, 4, 5, 6).collect(Collectors.summingInt(n -> n)));
     }
 
     /**
@@ -236,7 +234,99 @@ public class StreamTerminalOperationCollectorPractice {
      */
     @Test
     public void minBy(){
-        //使用
+        //使用collector获取最小元素
+        log.info("[1, 2, 3, 4, 5, 6]的最小值：{}", Stream.of(1, 2, 3, 4, 5, 6).collect(Collectors.minBy(Integer::compareTo)).get());
     }
 
+    /**
+     * 统计最大元素maxBy()的使用
+     */
+    @Test
+    public void maxBy(){
+        //使用collector获取最大元素
+        log.info("[1, 2, 3, 4, 5, 6]的最大值：{}", Stream.of(1, 2, 3, 4, 5, 6).collect(Collectors.maxBy(Integer::compareTo)).get());
+    }
+
+    /**
+     * 统计累加处理reducing的使用
+     * reducing的使用和Steam中的reduce()操作方法类似
+     */
+    @Test
+    public void reducing(){
+        //使用collector实现求和
+        log.info("[1, 2, 3, 4, 5, 6]的求和：{}", Stream.of(1, 2, 3, 4, 5, 6).collect(Collectors.reducing(0 , Integer::sum)));
+    }
+
+    /**
+     * 转换映射mapping的使用
+     * mapping支持将第一个参数的结果再次转换，即向下游传递
+     */
+    @Test
+    public void mapping(){
+        log.info("[1, 2, 3, 4, 5, 6]每个值加20后的平均值：{}", Stream.of(1, 2, 3, 4, 5, 6).collect(Collectors.mapping(n -> n + 20, Collectors.averagingInt(n -> n))));
+    }
+
+    /**
+     * 转换链接joining的使用
+     * joining提供了3种重载方法，支持传递 分隔符，前缀， 后缀等
+     */
+    @Test
+    public void joining(){
+        //使用collector连接字符串
+        log.info("连接字符串为：{}", Stream.of("a", "b", "c").collect(Collectors.joining(" ", "Java," ,"!")));
+        //连接字符串为：Java,a b c!
+    }
+
+    /**
+     * 装换为集合toList的使用
+     */
+    @Test
+    public void toList(){
+        log.info("[1, 2, 3, 4, 5, 6, 5, 3, 2]转换为集合：{}",
+                Stream.of(1, 2, 3, 4, 5, 6, 5, 3, 2).collect(Collectors.toList()));
+    }
+    /**
+     * 转换为Map toMap的使用
+     * toMap提供了3中重载方法，除了指定Key和Value的生成器外，区别在于对于Key重复时，Value的处理方式；以及初始Map的生成器
+     */
+    @Test
+    public void toMap(){
+        log.info("[1, 2, 3, 4, 5, 6, 5, 3, 2]转换为Map：{}",
+                Stream.of(1, 2, 3, 4, 5, 6, 5, 3, 2).collect(Collectors.toMap(Object::toString, n -> n , Integer::sum)));
+                //Integer::sum这个意思是当转换成map时，key键值冲突时候如何进行处理，这里的意思时当冲突时候，进行相加
+    }
+
+    /**
+     * 转换为Set toSet的使用
+     */
+    @Test
+    public void toSet(){
+        log.info("[1, 2, 3, 4, 5, 6, 5, 3, 2]转换为Set:{}",
+                Stream.of(1, 2, 3, 4, 5, 6, 5, 3, 2).collect(Collectors.toSet()));
+        //Set集合会进行除重处理
+    }
+
+    /**
+     * 转换为分组groupingBy的使用
+     * 分组函数将流中的元素按照某种定义分组，也提供了2种重载的方法，支持递归向下游分组
+     */
+    @Test
+    public void groupingBy(){
+        log.info("[1, 2, 3, 4, 5, 6, 5, 3, 2]的分组数据：{}",
+                Stream.of(1, 2, 3, 4, 5, 6, 5, 3, 2).collect(Collectors.groupingBy(n -> n)));
+    }
+
+    /**
+     * 转换为分区partitioningBy的使用
+     * 分区函数将流种元素分为2组，也提供了两种重载的方法，支持递归向下游分组
+     */
+    @Test
+    public void partitioningBy(){
+        log.info("[1, 2, 3, 4, 5, 6, 5, 3, 2]的奇偶分区数据：{}",
+                Stream.of(1, 2, 3, 4, 5, 6, 5, 3, 2).collect(Collectors.partitioningBy(n -> n % 2 == 0)));
+    }
+
+    /**
+     * Collectors 中还提供了 groupingByConcurrent 、 toCollection 、 toConcurrentMap 等几种支持并发的 Collector 实现，用法基本和非并发的相同，我们就不详述了
+     */
 }
