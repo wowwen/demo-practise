dcd723f3e52ab64a9bf412e1b8a5e97962c1cab6
jans9 <918418701@qq.com>
2024-09-28 04:48:38 +0800

    SpringBoot升级后Junit4换成Junit5，修改测试相关注解

--- demo-annotation/src/test/java/com/demo/annotation/test/MessageControllerTest.java
+++ demo-annotation/src/test/java/com/demo/annotation/test/MessageControllerTest.java
@@ -1,9 +1,11 @@
 package com.demo.annotation.test;
 
 import com.demo.annotation.springDI.primary.MessageController;
-import org.junit.Before;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+//import org.junit.Before;
+//import org.junit.Test;
+//import org.junit.runner.RunWith;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -12,16 +14,17 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
-@RunWith(SpringRunner.class)
+//@RunWith(SpringRunner.class)
 @SpringBootTest
-public class MessageControllerTest {
+class MessageControllerTest {
 
     @Autowired
     private MessageController messageController;
 
     private MockMvc mockMvc;
 
-    @Before
+//    @Before  Junit4使用此注解
+    @BeforeEach  //Junit5 换成了此注解
     public void setUp() {
         mockMvc = MockMvcBuilders.standaloneSetup(messageController).build();
     }
